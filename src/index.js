fd7de16409d761110c4724c42592089c820540ba
Jaron Rosenau <jaron.rosenau@gmail.com>
2019-07-05 01:41:01 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-const Qweb3 = require('./qweb3');
+const Hweb3 = require('./hweb3');
 const Encoder = require('./formatters/encoder');
 const Decoder = require('./formatters/decoder');
 const Utils = require('./utils');
 
 module.exports = {
-  Qweb3,
+  Hweb3,
   Encoder,
   Decoder,
   Utils,
