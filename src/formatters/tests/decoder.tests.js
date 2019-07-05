a05bcf9d3614f641d7d4f7cfe3d6467e39413f21
Jaron Rosenau <jaron.rosenau@gmail.com>
2019-07-05 02:50:53 -0700

    Update decoder.tests.js

--- src/formatters/tests/decoder.tests.js
+++ src/formatters/tests/decoder.tests.js
@@ -7,25 +7,25 @@ const Decoder = require('../decoder');
 const { assert, expect } = chai;
 
 describe('Decoder', () => {
-  describe('toQtumAddress()', () => {
-    it('returns the converted qtum address', () => {
+  describe('toHtmlcoinAddress()', () => {
+    it('returns the converted htmlcoin address', () => {
       assert.equal(
-        Decoder.toQtumAddress('17e7888aa7412a735f336d2f6d784caefabb6fa3', false),
-        'qKjn4fStBaAtwGiwueJf9qFxgpbAvf1xAy',
+        Decoder.toHtmlcoinAddress('17e7888aa7412a735f336d2f6d784caefabb6fa3', false),
+        'hKjn4fStBaAtwGiwueJf9qFxgpbAvf1xAy',
       );
       assert.equal(
-        Decoder.toQtumAddress('2a2ad24849bc061f0f7abee243ebdb584b0d11f1', true),
-        'QQSwne4oB1jmRXceHrs9tPGQmn7qjvSqyR',
+        Decoder.toHtmlcoinAddress('2a2ad24849bc061f0f7abee243ebdb584b0d11f1', true),
+        'HQSwne4oB1jmRXceHrs9tPGQmn7qjvSqyR',
       );
     });
 
     it('throws if hexAddress is undefined or empty', () => {
-      expect(() => Decoder.toQtumAddress()).to.throw();
-      expect(() => Decoder.toQtumAddress('')).to.throw();
+      expect(() => Decoder.toHtmlcoinAddress()).to.throw();
+      expect(() => Decoder.toHtmlcoinAddress('')).to.throw();
     });
 
     it('throws if hexAddress is not hex', () => {
-      expect(() => Decoder.toQtumAddress('qKjn4fStBaAtwGiwueJf9qFxgpbAvf1xAy')).to.throw();
+      expect(() => Decoder.toHtmlcoinAddress('hKjn4fStBaAtwGiwueJf9qFxgpbAvf1xAy')).to.throw();
     });
   });
 
